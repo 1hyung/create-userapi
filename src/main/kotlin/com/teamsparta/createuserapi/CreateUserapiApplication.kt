b5bd043544a13a0e3cffaad57cc67f00caf31e99
1hyung <ryu1hyung@naver.com>
2024-05-22 22:54:59 +0900

    feat: set up initial Spring Boot project
    - Create Spring Boot application with initial dependencies
    - Add .gitignore file
    - Add initial README.md

---
+++ src/main/kotlin/com/teamsparta/createuserapi/CreateUserapiApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.createuserapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreateUserapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<CreateUserapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/teamsparta/createuserapi/CreateUserapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.createuserapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreateUserapiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
