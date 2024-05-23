62882a55e83ed240ca432d988bb9f3096de33ba1
1hyung <ryu1hyung@naver.com>
2024-05-23 11:56:42 +0900

    feat: Integrate Swagger for API documentation
    - Added Swagger configuration class (SwaggerConfig) to document User API.
    - Configured OpenAPI with basic information such as title and version.
    - Imported necessary Swagger dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ src/main/kotlin/com/teamsparta/createuserapi/infra/swagger/SwaggerConfig.kt
@@ -0,0 +1,14 @@
+package com.teamsparta.createuserapi.infra.swagger
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun openAPI(): OpenAPI = OpenAPI().components(Components()).info(Info().title("User API").version("v1.0.0"))
+}
